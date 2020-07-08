42971a7934a46681babc8be276b63daa09bc3444
dxfyoy <dxfyoy@163.com>
2020-07-08 22:27:52 +0800

    第一次向github提交代码

---
+++ demo01.py
@@ -0,0 +1,45 @@
+# print("hello world!")  #字符串
+# print(2333)    #整数
+# print(2.333)    #小数  浮点数
+# print(True)     #布尔值    True  False
+# print(())       # 元组 
+# print([])       #数组
+# print({})       #字典
+# """
+# 大鹏一日同风起
+# 扶摇直上九万里
+# """
+# print('haha',2333,2.333)
+# print("哈哈" + "嘻嘻")   #字符串的拼接
+# print("哈"*100)
+# print(2<3)
+
+# #变量   赋值
+# name = '张三'   #把张三这个值赋值给了名字叫name的这个变量
+# print(name)
+
+# a = input("请输入：")    #input接收到的数据是字符串类型  要做整数的加减  需要做类型转换
+# print("input获取的内容：",a)
+
+#数据格式的转换
+# print(type('haha'))
+# print(type(2333))
+# print(type(2.333))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+# a = str(2333)
+# print(type(a))
+
+# a = int(input("请输入："))
+# b = int(input("请输入："))
+# print("input获取的内容：",a+b)
+
+
+
+
+
+
+
